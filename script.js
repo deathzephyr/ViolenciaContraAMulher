f07c2faad0223b3b4329396d989f774647734c37
deathzephyr <deathzephyr111@gmail.com>
2026-02-11 21:44:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const navLinks = document.getElementById('navLinks');
     navLinks.classList.toggle('active');
 }
+
+function toggleControls() {
+    const controlMenu = document.getElementById('controlMenu');
+    controlMenu.classList.toggle('active');
+}

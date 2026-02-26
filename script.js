7d4c17d131d52e2eacd4f1182c14c3627336adb5
deathzephyr <deathzephyr111@gmail.com>
2026-02-26 11:09:59 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.getElementById('navLinks');
-    navLinks.classList.toggle('active');
-}
-
-function toggleControls() {
-    const controlMenu = document.getElementById('controlMenu');
-    controlMenu.classList.toggle('active');
-}
